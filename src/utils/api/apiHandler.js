6e690768530525cba4911acd1cabdb05dd90605d
gaurav.verma <gaurav.verma@navyuginfo.com>
2020-08-03 23:51:12 +0530

    commented token line

--- src/utils/api/apiHandler.js
+++ src/utils/api/apiHandler.js
@@ -4,7 +4,7 @@ import { BASE_URL } from "../constants";
 const apiHandler = {
     
     getDataAPI: (url, params) => {
-        const token = localStorage.getItem('chem-token');
+        // const token = localStorage.getItem('chem-token');
         // if (token) {
             return proxy
                 .call("get", url, params, null)
@@ -32,7 +32,7 @@ const apiHandler = {
     },
 
     postDataAPI: (url, body) => {
-        const token = localStorage.getItem('chem-token');
+        // const token = localStorage.getItem('chem-token');
         // if (token) {
             return proxy
                 .call("post", `${BASE_URL}${url}`, null, body, null)
